8ac75d1826a49754e4b81c2a599e83cb9859fabf
junreycloudstrife <junrey.cloudstrife@gmail.com>
2023-01-13 14:16:39 +0800

    Re-add SecondComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { FirstComponent } from './first/first.component';
-// import { SecondComponent } from './second/second.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // FirstComponent,
-    // SecondComponent
+    SecondComponent
   ],
   imports: [
     BrowserModule,
