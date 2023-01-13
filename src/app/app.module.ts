86d40717e987d2898b30eeb41144e07e15a68e98
junreycloudstrife <junrey.cloudstrife@gmail.com>
2023-01-13 14:09:06 +0800

    Created components
    Created FirstComponent and SecondComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
